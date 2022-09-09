7fe31e8977fc84182942956e64c488e1ba639751
fiskgrodan <jam@Johans-MacBook-Pro.local>
2022-09-09 14:02:34 +0200

    feat: make orange

---
+++ src/App.svelte
@@ -0,0 +1,46 @@
+<script>
+  import Summer from "./components/Summer.svelte";
+</script>
+
+<main>
+  <h1>Svelte static</h1>
+  <Summer />
+</main>
+
+<style>
+  :global(:root) {
+    font-family: "OpenDyslexic", sans-serif;
+    font-size: 16px;
+    line-height: 24px;
+    font-weight: 400;
+
+    color-scheme: normal;
+    color: orange;
+    background-color: black;
+
+    font-synthesis: none;
+    text-rendering: optimizeLegibility;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-text-size-adjust: 100%;
+  }
+
+  :global(html, body) {
+    margin: 0;
+    padding: 0;
+  }
+
+  :global(::selection) {
+    background-color: orange;
+    color: black;
+  }
+
+  main {
+    padding: 20px 40px;
+  }
+
+  h1 {
+    margin-block-start: 20px;
+    margin-block-end: 20px;
+  }
+</style>
---
+++ src/components/Summer.svelte
@@ -0,0 +1,33 @@
+<script>
+  let left = 0;
+  let right = 0;
+
+  $: sum = left + right;
+</script>
+
+<section>
+  <div><input bind:value={left} type="number" /></div>
+  <div><input bind:value={right} type="number" /></div>
+  <div>{sum}</div>
+</section>
+
+<style>
+  input {
+    background-color: black;
+    color: darkorange;
+    border: 1px solid black;
+    box-shadow: none;
+    outline: none;
+    font-family: "OpenDyslexic", sans-serif;
+    font-size: 16px;
+    padding: 0;
+    margin: 0 0 10px 0;
+  }
+
+  input[type="number"]::-webkit-inner-spin-button,
+  input[type="number"]::-webkit-outer-spin-button {
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    appearance: none;
+  }
+</style>
